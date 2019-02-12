899f94542ce7c4c9f9775e2393bd10c833ae1b51
orache arthur <arthur.orache@gmail.com>
2019-02-12 18:23:40 +0300

    Add user login  - Create login view  - Write tests  [Finishes #163501105]

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -11,7 +11,6 @@ class TestAuth(BaseTestCase):
         """
         Test for successful user registration
         """
-
         response = self.register_user('test', 'testing', 'test')
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'success')
@@ -36,10 +35,44 @@ class TestAuth(BaseTestCase):
         """
         Test for Incomplete input during registration
         """
-
         response = self.register_user('test', 'test')
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'fail')
         self.assertTrue(data['message'] == 'Incomplete data. Username, name and '
                         'password must be provided')
+        self.assertEqual(response.status_code, 400)
+
+    def test_login_successful(self):
+        """
+        Test for successful user registration
+        """
+        response1 = self.register_user('test', 'testing', 'test')
+        response = self.login_user('test', 'testing')
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'success')
+        self.assertTrue(data['message'] == 'Successfully logged in.')
+        self.assertTrue(data['auth_token'])
+        self.assertEqual(response.status_code, 200)
+
+    def test_non_registered_user_login(self):
+        """
+        Test that login of a non-registered user fails
+        """
+        response = self.login_user('test', 'test')
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'fail')
+        self.assertTrue(data['message'] == 'User does not exist.')
+        self.assertTrue(response.content_type == 'application/json')
+        self.assertEqual(response.status_code, 401)
+
+    def test_login_with_missing_credentials(self):
+        """
+        Test that a user cannot login with missing credentials
+        """
+        response = self.login_user('test')
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'fail')
+        self.assertTrue(data['message'] ==
+                        'Username or password not provided.')
+        self.assertTrue(response.content_type == 'application/json')
         self.assertEqual(response.status_code, 400)
