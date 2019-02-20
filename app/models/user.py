f58bdff8b61da98bf8ea0c3ea3a86367d3553fff
orache arthur <arthur.orache@gmail.com>
2019-02-21 02:09:46 +0300

    Add test for image upload

--- app/models/user.py
+++ app/models/user.py
@@ -43,7 +43,7 @@ class User(ModelMixin):
         """
         try:
             payload = {
-                'exp': datetime.utcnow() + timedelta(hours=4),
+                'exp': datetime.utcnow() + timedelta(hours=12),
                 'iat': datetime.utcnow(),
                 'sub': user_id
             }
